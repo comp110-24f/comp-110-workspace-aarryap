d62c29861911bc9675e7357a0b48ff90ee3afddb
Aarrya Pillai <aarryap@unc.edu>
2024-10-14 23:27:38 -0400

    Added notes on lists!

---
+++ lessons/lists.py
@@ -0,0 +1,26 @@
+"""Basic syntax for lists"""
+
+# What is difference between literal and constructor?
+
+# How to create an empty list
+my_numbers: list[float] = []  # literal for empty list
+my_numbers: list[float] = list()  # constructor for empty list
+
+print(my_numbers)
+
+# String Analogy
+# my_name: str = "" # literal
+# my_name:str = str() # constructor
+
+# how to add something to a list
+# <list name>.append(<item>)
+# grocery_list.append(“bananas”)
+
+# Addding a value to a list
+my_numbers.append(1.5)
+my_numbers.append(2.3)
+print(my_numbers)
+
+# Creating an already populate ____??
+game_points: list[int] = [102, 86, 94]
+# finish watchin levture 9/30 and finish CQ and Lesson
