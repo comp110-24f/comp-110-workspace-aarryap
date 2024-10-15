79f8bd6d5458d6b84a96eb8b83da6871aec0c60d
Aarrya Pillai <aarryap@unc.edu>
2024-10-14 23:27:25 -0400

    Added to notes!

--- lessons/random_notes.py
+++ lessons/random_notes.py
@@ -1,3 +1,8 @@
 # Use python -m tools.submission folder/file_name.py to save as zip file
 # Use python -m folder.file_name (without .py) to run a file
 # index is just a value at a certain part
+# unit test file ends in _test.py
+# unit test function begins in test_
+# one assert statement that evals to bool per unit test
+# write following in terminal to run a unit test w/o test tube
+# python -m pytest path/to/testfile.py
